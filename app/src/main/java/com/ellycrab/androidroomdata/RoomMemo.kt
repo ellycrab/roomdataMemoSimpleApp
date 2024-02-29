9040fd163e206b894d58b63d04783c97d773aa7d
ellycrab <54714275+ellycrab@users.noreply.github.com>
2024-02-29 17:28:36 +0900

    Update RoomMemo.kt

--- app/src/main/java/com/ellycrab/androidroomdata/RoomMemo.kt
+++ app/src/main/java/com/ellycrab/androidroomdata/RoomMemo.kt
@@ -9,7 +9,7 @@ import java.time.chrono.ChronoLocalDateTime
 @Entity(tableName = "room_memo")
 class RoomMemo {
 
-    @PrimaryKey(autoGenerate = true) //no에 값이 없을 때 자동증가된 숫자값을 db에 입력해주ㅜㄴ다.
+    @PrimaryKey(autoGenerate = true) //no에 값이 없을 때 자동증가된 숫자값을 db에 입력해준다
     @ColumnInfo
     var no: Long? = null
 
